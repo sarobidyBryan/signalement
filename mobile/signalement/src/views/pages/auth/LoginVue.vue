7e0cb2e089fd4c5dc0f1ffb2988476badb14665a
NyAinaMickael <ermiteandrianarivelo@gmail.com>
2026-01-27 07:59:16 +0300

    style: amelioration ux login avec chargement apres clic sur bouton

--- mobile/signalement/src/views/pages/auth/LoginVue.vue
+++ mobile/signalement/src/views/pages/auth/LoginVue.vue
@@ -13,7 +13,7 @@ import {
   IonCol,
   IonIcon 
 } from '@ionic/vue';
-import { doc, getDoc, collection, query, where, getDocs, setDoc, updateDoc, increment, deleteDoc } from 'firebase/firestore';
+import { doc, getDoc, collection, query, where, getDocs, setDoc, updateDoc, increment } from 'firebase/firestore';
 
 export default defineComponent({
   name: 'LoginPage',
@@ -33,7 +33,8 @@ export default defineComponent({
     return {
       email: '',
       password: '',
-      errorMessage: ''
+      errorMessage: '',
+      isLoading: false
     };
   },
   
@@ -54,6 +55,7 @@ export default defineComponent({
       }
       
       try {
+        this.isLoading = true ;
         const userCred = await signInWithEmailAndPassword(auth, this.email, this.password);
         const uid = userCred.user.uid;
 
@@ -196,6 +198,9 @@ export default defineComponent({
             this.errorMessage = 'Une erreur est survenue. Veuillez réessayer.';
         }
       }
+      finally{
+        this.isLoading = false;
+      }
     }
   }
 });
@@ -269,9 +274,17 @@ export default defineComponent({
                 type="submit"
                 expand="block"
                 class="font-medium h-12"
-                color="dark" 
+                color="dark"
+                :disabled="isLoading" 
               >
-                Se connecter
+                <span v-if="!isLoading">Se connecter</span>
+                <span v-else class="flex items-center justify-center">
+                  <svg class="animate-spin h-5 w-5 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  Connexion...
+                </span>
               </ion-button>
 
               <!-- Lien d'inscription -->
