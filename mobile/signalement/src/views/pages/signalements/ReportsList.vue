fc32a9cf2849e32ec2a4d3d05de4ded9522c2ddf
Randie Ranaivoson <randieranaivoson@gmail.com>
2026-02-10 10:27:04 +0300

    refactor: ajout de champ level dans la collection

--- mobile/signalement/src/views/pages/signalements/ReportsList.vue
+++ mobile/signalement/src/views/pages/signalements/ReportsList.vue
@@ -345,6 +345,9 @@ export default defineComponent({
         // Ajouter les timestamps
         reportData.createdAt = new Date();
         reportData.updatedAt = new Date();
+        
+        // Ajouter le champ level vide
+        reportData.level = "";
 
         // 1. Créer le document dans Firestore
         const docRef = await addDoc(collection(db, "reports"), reportData);
